d3493f4b9df15fc5a092568ab78b2958697d77b3
TenScoops <p.ayres9@gmail.com>
2024-09-21 06:26:49 -0400

    addition of invalid message content

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ function isEmailValid(email){
 function handleSubmit(event){
     event.preventDefault();
     const value = document.getElementById('email-input').value
-    
 
-    if(!isEmailValid(value)){//if not valid
-        document.getElementById('email-input').style.borderColor='red'
-        document.getElementById('email-input').style.backgroundColor='red'
-    }else{//valid
+    if(isEmailValid(value)){//if valid
         document.getElementById('email-input').style.backgroundColor='green'
+       
+    }else{//not valid
+         document.getElementById('email-input').style.borderColor='red'
+        document.getElementById('email-input').style.backgroundColor='red'
+        document.getElementById('error-message').style.display = 'inline'
     }
 }
 
